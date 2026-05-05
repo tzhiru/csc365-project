46f2c674b8b56bd32ec792978264e781d6171392
tzhiru <antnachtung@gmail.com>
2026-05-04 23:12:46 -0700

    very basic catalog retrieval (havent tested yet)

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -6,31 +6,10 @@ router = APIRouter()
 
 
 class CatalogItem(BaseModel):
-    sku: Annotated[str, Field(pattern=r"^[a-zA-Z0-9_]{1,20}$")]
-    name: str
-    quantity: Annotated[int, Field(ge=1, le=10000)]
-    price: Annotated[int, Field(ge=1, le=500)]
-    potion_type: List[int] = Field(
-        ...,
-        min_length=4,
-        max_length=4,
-        description="Must contain exactly 4 elements: [r, g, b, d]",
-    )
-
-
-# Placeholder function, you will replace this with a database call
-def create_catalog() -> List[CatalogItem]:
-    return [
-        CatalogItem(
-            sku="RED_POTION_0",
-            name="red potion",
-            quantity=1,
-            price=50,
-            potion_type=[100, 0, 0, 0],
-        )
-    ]
-    print("Test Update")
-
+    title: str
+    author_first: str
+    author_last: str
+    date_published: str
 
 @router.get("/catalog/", tags=["catalog"], response_model=List[CatalogItem])
 def get_catalog() -> List[CatalogItem]:
@@ -38,4 +17,31 @@ def get_catalog() -> List[CatalogItem]:
     Retrieves the catalog of items. Each unique item combination should have only a single price.
     You can have at most 6 potion SKUs offered in your catalog at one time.
     """
-    return create_catalog()
+    newCatalog: List[CatalogItem] = []
+
+    with db.engine.begin() as connection:
+        bk = connection.execute(
+            sqlalchemy.text(
+                """
+                SELECT books.title, authors.first_name as f, authors.last_name as l,
+                date_published
+                FROM books
+                JOIN authors on books.author_id = authors.id
+                ORDER BY books.title ASC
+                """
+            )
+        )
+        
+        # later actually have it based on what copies of books are actually in store, display how many copies avaliable.
+        
+        for bk in res:
+            newCatalog.append(
+                CatalogItem(
+                    title=bk.title,
+                    author_first=bk.f,
+                    author_last=bk.l,
+                    date_published=bk.date_published
+                )
+            )
+
+    return newCatalog
