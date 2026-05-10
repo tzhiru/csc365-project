3fce0bae6a4f6359ae459cc9e098d90e66a9a9f7
tzhiru <antnachtung@gmail.com>
2026-05-10 12:55:36 -0700

    fixed error

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from typing import List, Optional
 import sqlalchemy
