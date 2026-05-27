ea699b246c0ae446dd2ea95282da7d45c21ef3cc
jnne-lbg <68482222+jnne-lbg@users.noreply.github.com>
2026-05-26 21:54:43 -0700

    Setting Isolation Level to Serializable

--- src/database.py
+++ src/database.py
@@ -2,4 +2,4 @@ from src import config
 from sqlalchemy import create_engine
 
 connection_url = config.get_settings().POSTGRES_URI
-engine = create_engine(connection_url, pool_pre_ping=True)
+engine = create_engine(connection_url, pool_pre_ping=True, isolation_level="SERIALIZABLE")
